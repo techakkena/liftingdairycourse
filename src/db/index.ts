ae0e5b70e6e7be5dd6b7b133e5b6aff4aaaa2aa2
GYANABHASKAR <45270010+GYANABHASKAR@users.noreply.github.com>
2026-04-22 19:10:43 +0530

    initial commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,14 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
-const db = drizzle(process.env.DATABASE_URL!, { schema });
+const createDb = () => drizzle(process.env.DATABASE_URL!, { schema });
+type DbType = ReturnType<typeof createDb>;
 
-export { db };
+let _db: DbType | undefined;
+
+export const db = new Proxy({} as DbType, {
+  get(_, prop: string | symbol) {
+    if (!_db) _db = createDb();
+    return _db[prop as keyof DbType];
+  },
+});
